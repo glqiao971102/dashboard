22d65225fd32aec5f1d89524d6e5b19c8160f0d1
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-06-13 13:00:12 +0800

    initial

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const alertBanner = document.getElementById("alert");
+// create the html for the banner
+alertBanner.innerHTML =
+`
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+    }
+})
+
+//form section
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', (e) => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending")
+    e.preventDefault();
+    } else if (user.value === "" ) {
+    alert("Please fill out user field before sending")
+    e.preventDefault();
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending")
+    e.preventDefault();
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
+    });
